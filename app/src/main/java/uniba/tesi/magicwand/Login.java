19d9175b135d5f1a948f8503740f73d1da3a0179
rippfc <antonio.ricchetti92@gmail.com>
2020-06-11 03:37:28 +0200

    Layout Login, Register and Reset

---
+++ app/src/main/java/uniba/tesi/magicwand/Login.java
@@ -0,0 +1,39 @@
+package uniba.tesi.magicwand;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    private TextView txToRegister,txRememberPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        txToRegister=(TextView) findViewById(R.id.text_To_Register);
+        txRememberPass=(TextView)findViewById(R.id.text_To_reset);
+
+        txToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,Register.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        });
+
+        txRememberPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,ResetPassword.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        });
+
+    }
+}
